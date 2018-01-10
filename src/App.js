694e09023e1cfb3d65eddb28be15ba79bffec8b9
kutay <kutaykoc@outlook.com>
2018-01-10 16:12:38 +0300

    ekleme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ Autor: kutaykoc
  */
 
 import React, { Component } from 'react';
@@ -9,23 +7,19 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
+import List from './component/List'
 export default class App extends Component {
+ 
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          baslangic
-        </Text>
+      <View style={{flex:1}}>
+          <List />
       </View>
     );
   }
---
+++ src/component/List.js
@@ -0,0 +1,139 @@
+/**
+ Autor: kutaykoc
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image
+} from 'react-native';
+
+const shows_first=[
+    {
+        "key":1,
+        "name":"Suits",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/0/2432.jpg"
+     },
+     {
+        "key":2,
+        "name":"Modern Family",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/0/628.jpg"
+     },
+     {
+        "key":3,
+        "name":"The Flash",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/78/195988.jpg"
+     },
+     {
+        "key":4,
+        "name":"Supergirl",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/83/209955.jpg"
+     },
+     {
+        "key":5,
+        "name":"Designated Survivor",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/101/253490.jpg",
+     },
+     {
+        "key":6,
+        "name":"24: Legacy",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/90/225030.jpg"
+     },
+     {
+        "key":7,
+        "name":"Colony",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/91/229234.jpg"
+     }
+]
+
+const shows_second=[
+    {
+        "key":8,
+        "name":"The Walking Dead",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/67/168817.jpg"
+     },
+     {
+        "key":9,
+        "name":"Taken",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/100/250528.jpg"
+     },
+     {
+        "key":10,
+        "name":"This is us",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/70/175831.jpg"
+     },
+     {
+        "key":11,
+        "name":"Superstore",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/69/174909.jpg"
+     },
+     {
+        "key":12,
+        "name":"Lethal Weapon",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/93/234808.jpg"
+     },
+     {
+        "key":13,
+        "name":"The 100",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/94/236401.jpg"
+     },
+     {
+        "key":14,
+        "name":"Homeland",
+        "image":"https://static.tvmaze.com/uploads/images/medium_portrait/101/254425.jpg"
+     }
+]
+export default class List extends Component {
+  _renderItem(item){
+    return(
+        <Image style={{width:120,height:180}} source={{uri:item.image}}/>
+    )
+  }
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <View>
+            <Text>My List</Text>
+            <FlatList 
+            horizontal
+            ItemSeparatorComponent={()=><View style={{width:20}}></View>}
+            renderItem={({item})=>this._renderItem(item)}
+            data={shows_first}
+            />
+        </View>
+        <View>
+            <Text>For You</Text>
+            <FlatList 
+            horizontal
+            ItemSeparatorComponent={()=><View style={{width:20 }}></View>}
+            renderItem={({item})=>this._renderItem(item)}
+            data={shows_second}
+            />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
